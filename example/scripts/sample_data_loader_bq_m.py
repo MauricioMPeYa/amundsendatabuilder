25a72b80249da8166e7402886a0f0760d2b37802
MauricioMPeYa <56884998+MauricioMPeYa@users.noreply.github.com>
2020-07-24 09:41:20 -0300

    extractor para bq

--- example/scripts/sample_data_loader_bq_m.py
+++ example/scripts/sample_data_loader_bq_m.py
@@ -51,6 +51,8 @@ from databuilder.extractor.hive_table_metadata_extractor import *
 
 from databuilder.extractor.hive_table_last_updated_extractor import *
 
+from databuilder.extractor.bigquery_metadata_extractor import *
+
 #import hdfs3
 
 es_host = os.getenv('CREDENTIALS_ELASTICSEARCH_PROXY_HOST', 'localhost')
@@ -211,6 +213,52 @@ def create_last_updated_job():
                       publisher=Neo4jCsvPublisher())
 
 
+def run_bq_job(job_name):
+    
+    #where_clause_suffix = " "
+    gcloud_project = "peya-data-pocs"
+    #label_filter = ""
+
+    tmp_folder = '/var/tmp/amundsen/{job_name}'.format(job_name=job_name)
+    node_files_folder = '{tmp_folder}/nodes'.format(tmp_folder=tmp_folder)
+    relationship_files_folder = '{tmp_folder}/relationships'.format(tmp_folder=tmp_folder)
+
+
+    job_config = ConfigFactory.from_dict({
+	    'extractor.bigquery_table_metadata.{}'.format(
+            BigQueryMetadataExtractor.PROJECT_ID_KEY
+            ): gcloud_project,
+        'loader.filesystem_csv_neo4j.node_dir_path': node_files_folder,
+        'loader.filesystem_csv_neo4j.relationship_dir_path': relationship_files_folder,
+        'loader.filesystem_csv_neo4j.delete_created_directories': True,
+        'publisher.neo4j.node_files_directory': node_files_folder,
+        'publisher.neo4j.relation_files_directory': relationship_files_folder,
+        'publisher.neo4j.neo4j_endpoint': neo4j_endpoint,
+        'publisher.neo4j.neo4j_user': neo4j_user,
+        'publisher.neo4j.neo4j_password': neo4j_password,
+        'publisher.neo4j.neo4j_encrypted': False,
+        'publisher.neo4j.job_publish_tag': 'unique_tag',  # should use unique tag here like {ds}     
+        })
+
+    #if label_filter:
+    #    job_config[
+    #        'extractor.bigquery_table_metadata.{}'
+    #        .format(BigQueryMetadataExtractor.FILTER_KEY)
+    #        ] = label_filter
+    
+    task = DefaultTask(extractor=BigQueryMetadataExtractor(),
+                       loader=FsNeo4jCSVLoader(),
+                       transformer=NoopTransformer())
+
+    job = DefaultJob(conf=ConfigFactory.from_dict(job_config),
+                     task=task,
+                     publisher=Neo4jCsvPublisher())
+
+    job.launch()
+
+
+
+
 if __name__ == "__main__":
     # Uncomment next line to get INFO level logging
     # logging.basicConfig(level=logging.INFO)
@@ -221,7 +269,10 @@ if __name__ == "__main__":
         #run_csv_job('example/sample_data/sample_table_column_stats.csv', 'test_table_column_stats',
         #            'databuilder.models.table_stats.TableColumnStats')
         print("EMPIEZA A CORRER EL JOB...")
-        run_hive_job("test_hive_mauricio")
+
+        run_bq_job("test_bq")
+        #run_hive_job("test_hive_mauricio")
+
         print("TERMINA DE CORRER EL JOB...")
 
         job_es_table = create_es_publisher_sample_job(
@@ -231,7 +282,7 @@ if __name__ == "__main__":
             model_name='databuilder.models.table_elasticsearch_document.TableESDocument')
         job_es_table.launch()
 
-        create_last_updated_job().launch()
+        #create_last_updated_job().launch()
 
 
         
