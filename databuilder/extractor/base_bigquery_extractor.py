e0b2c2653be682085cfacb0daca862dbcf343609
MauricioMPeYa <56884998+MauricioMPeYa@users.noreply.github.com>
2020-08-27 12:55:35 -0300

    Update base_bigquery_extractor.py

--- databuilder/extractor/base_bigquery_extractor.py
+++ databuilder/extractor/base_bigquery_extractor.py
@@ -109,7 +109,7 @@ class BaseBigQueryExtractor(Extractor):
         i=0
         while i < len(response['datasets']) and not found :
             found = (response['datasets'][i]['datasetReference']['datasetId'] == 'General_Tables')
-            found = found or (response['datasets'][i]['datasetReference']['datasetId'] == 'Checkout')
+            #found = found or (response['datasets'][i]['datasetReference']['datasetId'] == 'Checkout')
             if found : filtered_dataset.append(response['datasets'][i])
             i+=1
         response['datasets'] = filtered_dataset
